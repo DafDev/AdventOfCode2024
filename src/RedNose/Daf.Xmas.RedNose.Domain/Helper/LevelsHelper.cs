a6b9595146db948d7d5dfca5f594a453bed7d6fa
Daf <afahd.ali@gmail.com>
2024-12-20 16:00:19 +0100

    feat: update code and displace levels checking into helper class

---
+++ src/RedNose/Daf.Xmas.RedNose.Domain/Helper/LevelsHelper.cs
@@ -0,0 +1,32 @@
+﻿namespace Daf.Xmas.RedNose.Domain.Helper;
+
+public static class LevelsHelper
+{
+    public static bool CheckLevelsAreEvolvingWithinSteps(IEnumerable<int> levels, int minimumDistance, int maximumDistance ,out int problematicIndex)
+    {
+        foreach (var level in levels.Index())
+        {
+            if (level.Index + 1 >= levels.Count())
+            {
+                problematicIndex = -1; 
+                return true;
+            }
+            
+            var nextLevel = levels.ElementAtOrDefault(level.Index + 1);
+            var previousLevel = levels.ElementAtOrDefault(level.Index == 0 ? 0 : level.Index - 1);
+
+            if (Math.Abs(level.Item - nextLevel) > maximumDistance ||
+                Math.Abs(level.Item - nextLevel) < minimumDistance
+                || level.Index > 0 && (Math.Sign(previousLevel - level.Item) != Math.Sign(level.Item - nextLevel)))
+            {
+                
+                var nextNextLevel = levels.ElementAtOrDefault(level.Index + 2);
+                problematicIndex = level.Index;
+                return false;
+            }
+        }
+                
+        problematicIndex = -1; 
+        return true;
+    }
+}
--- src/RedNose/Daf.Xmas.RedNose.Domain/Models/Report.cs
+++ src/RedNose/Daf.Xmas.RedNose.Domain/Models/Report.cs
@@ -1,4 +1,6 @@
-﻿namespace Daf.Xmas.RedNose.Domain.Models;
+﻿using Daf.Xmas.RedNose.Domain.Helper;
+
+namespace Daf.Xmas.RedNose.Domain.Models;
 
 public class Report
 {
@@ -7,26 +9,7 @@ public class Report
     private int _problematicIndex = -1;
     public IEnumerable<int> Levels { get; set; } = [];
 
-    public bool IsSafe()
-    {
-        foreach (var level in Levels.Index())
-        {
-            if (level.Index + 1 >= Levels.Count()) return true;
-            
-            var nextLevel = Levels.ElementAtOrDefault(level.Index + 1);
-            var previousLevel = Levels.ElementAtOrDefault(level.Index == 0 ? 0 : level.Index - 1);
-
-            if (Math.Abs(level.Item - nextLevel) > MaximumDistance ||
-                Math.Abs(level.Item - nextLevel) < MinimumDistance
-                || level.Index > 0 && (Math.Sign(previousLevel - level.Item) != Math.Sign(level.Item - nextLevel)))
-            {
-                _problematicIndex = level.Index;
-                return false;
-            }
-        }
-        
-        return true;
-    }
+    public bool IsSafe() => LevelsHelper.CheckLevelsAreEvolvingWithinSteps(Levels, MinimumDistance, MaximumDistance ,out _problematicIndex);
 
     public static Report FromDto(ReportDto reportDto) => new() { Levels = [..reportDto.Levels] };
 
@@ -35,19 +18,6 @@ public class Report
         if (IsSafe()) return true;
         var changedLevel = Levels.ToList();
         changedLevel.RemoveAt(_problematicIndex); 
-        if (!(changedLevel.SequenceEqual(changedLevel.Order()) || changedLevel.SequenceEqual(changedLevel.OrderByDescending(x => x)))) 
-            return false;
-        foreach (var level in changedLevel.Index())
-        {
-            if (level.Index + 1 >= changedLevel.Count) return true;
-            
-            var nextLevel = changedLevel.ElementAtOrDefault(level.Index + 1);
-            if (Math.Abs(level.Item - nextLevel) > MaximumDistance ||
-                Math.Abs(level.Item - nextLevel) < MinimumDistance)
-            {
-                return false;
-            }
-        }
-        return true;
+        return LevelsHelper.CheckLevelsAreEvolvingWithinSteps(changedLevel, MinimumDistance, MaximumDistance, out _problematicIndex);
     }
 }
